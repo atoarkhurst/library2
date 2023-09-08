40dc19dd7df72f021688aa1473e41040f8a1de05
Ato Arkhurst <atoparkhurst@gmail.com>
2023-09-08 19:36:51 -0400

    Add new books to library and display in grid

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-
+// vars
 const newBookBtn = document.querySelector('.newBookBtn');
 const modal = document.querySelector('.modal');
 const form = document.getElementById('bookForm');
+const bookGrid = document.querySelector('.book-grid');
+const myLibrary = [];
 
+// open add book form on click 
 newBookBtn.addEventListener('click', function() {
     modal.showModal();
 });
 
-form.addEventListener('subm,it', function(event) {
+// get user data on submit
+form.addEventListener('submit', function(event) {
+
+  clearGrid();
+  event.preventDefault()
+  modal.close();
+
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const status = document.getElementById('status').checked ? 'read' : 'unread';
+
+  addBookToLibrary(title, author, pages, status);
+  populateDisplay(myLibrary);
+
+  
+  form.reset();
     
 })
 
 
-
-
-const myLibrary = [];
-
+// book object constructor
 function Book(title, author, pages, status) {
-  // the constructor...
+
   this.title = title,
   this.author = author,
   this.pages = pages,
@@ -25,8 +41,9 @@ function Book(title, author, pages, status) {
 
 }
 
+// add book to library array
 function addBookToLibrary(title, author, pages, status) {
-  // do stuff here
+
     let book = new Book(title, author, pages, status);
     myLibrary.push(book);
 }
@@ -36,7 +53,6 @@ function populateDisplay(myLibrary) {
     //for each book in array
     myLibrary.forEach( (book) => {
         
-        let bookGrid = document.querySelector('.book-grid');
         
         //store vars
         const {title, author, pages, status } = book;
@@ -58,15 +74,14 @@ function populateDisplay(myLibrary) {
         cardPages.innerText = `Pages: ${pages}`;
 
         let cardStatus = document.createElement('p');
-        cardStatus.innerText = `Status: {cardStatus}`;
-
-
+        cardStatus.innerText = `Status: ${status}`;
 
 
         cardElement.appendChild(cardTitle);
         cardElement.appendChild(cardAuthor);
         cardElement.appendChild(cardPages);
         cardElement.appendChild(cardAuthor);
+        cardElement.appendChild(cardStatus);
         
 
 
@@ -77,18 +92,7 @@ function populateDisplay(myLibrary) {
 
 }
 
+function clearGrid() {
+  bookGrid.innerHTML = "";
+}
 
-//Test Data
-addBookToLibrary("Harry Potter", "J.K. Rowling", 364, "read");
-addBookToLibrary("The Great Gatsby", "F. Scott Fitzgerald", 180, "unread");
-addBookToLibrary("1984", "George Orwell", 328, "unread");
-addBookToLibrary("To Kill a Mockingbird", "Harper Lee", 281, "read");
-addBookToLibrary("The Catcher in the Rye", "J.D. Salinger", 224, "unread");
-addBookToLibrary("Pride and Prejudice", "Jane Austen", 279, "read");
-addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1178, "unread");
-addBookToLibrary("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 193, "read");
-addBookToLibrary("To the Lighthouse", "Virginia Woolf", 209, "unread");
-addBookToLibrary("The Da Vinci Code", "Dan Brown", 454, "read");
-
-
-populateDisplay(myLibrary);
